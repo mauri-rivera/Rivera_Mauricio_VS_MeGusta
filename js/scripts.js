aafcf7062d9ac4080e8d3c394586c62df083ead0
mauri-rivera <mauri.rivera@outlook.com>
2023-09-21 23:25:18 -0300

    Correcciones

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,34 @@ function sumarLike() {
     let numeroLike = document.querySelector(".cantidadLike");
     let textoLike = numeroLike.innerHTML;
 
-    if (contador >= 0 && contador <= 9) {
-        contador = parseInt(textoLike.substring(0, 1));
-        contador++;
-    }
-    else if (contador >= 10 && contador <= 99) {
-        contador = parseInt(textoLike.substring(0, 2));
-        contador++;
-    }
-    else if (contador >= 100 && contador <= 999) {
-        contador = parseInt(textoLike.substring(0, 3));
-        contador++;
-    }
+    let totalLike = contarLike(textoLike.length, textoLike);
     
-    document.querySelector(".cantidadLike").innerHTML = `${contador} like(s)`;
+    document.querySelector(".cantidadLike").innerHTML = `${totalLike} like(s)`;
+}
+
+function sumarLikePorUsuario(elemento) {
+    let identificador = elemento.id;
+    let botonLike = document.querySelector(`#${identificador}`);
+    let numeroLike = botonLike.previousElementSibling;
+    let textoLike = numeroLike.innerHTML;
+    let totalLike = contarLike(textoLike.length, textoLike);
+
+    numeroLike.innerHTML = `${totalLike} like(s)`;
+}
+
+function contarLike(cantidad, texto) {
+    if (cantidad === 9) {
+        contador = parseInt(texto.substring(0, 1));
+        contador++;
+    }
+    else if (cantidad === 10) {
+        contador = parseInt(texto.substring(0, 2));
+        contador++;
+    }
+    else if (cantidad === 11) {
+        contador = parseInt(texto.substring(0, 3));
+        contador++;
+    }
+
+    return contador;
 }
